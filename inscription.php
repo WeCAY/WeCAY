67d04c83c5b8961042d04ca6adf4bab2fabb1344
quentinaml <73106402+quentinaml@users.noreply.github.com>
2020-10-23 11:48:51 +0200

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -2,7 +2,7 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="CSS/inscription.css" />
+        <link rel="stylesheet" href="CSS/inscription1.css" />
         <title>Inscription | WeCAY</title>
     </head>
 
