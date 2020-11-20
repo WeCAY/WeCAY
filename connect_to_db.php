c8226b33f60a7c323c9ebcdd718435bf3dc3b2f5
lenzone91 <cognenzo@gmail.com>
2020-11-20 10:35:27 +0100

    connexion bdd

--- connect_to_db.php
+++ connect_to_db.php
@@ -1,10 +1,9 @@
 <?php
 
-
 function Connect()
 {
-    try {   //$db=mysqli_connect("127.0.0.1", "root", "", "tpje");
-        $db = new PDO('mysql:host=localhost:8889;dbname=wecay;charset=utf8', 'root', 'root');
+    try { 
+        $db = new PDO('mysql:host=mysql-wecay.alwaysdata.net;dbname=wecay_isep;charset=utf8', 'wecay', 'Isepwecay');
         return $db;
 
     } catch (Exception $e) {
