cb82683c601dc753e23fa54f07f61d278711ea3d
quentinaml <quentin.amiel@free.fr>
2021-02-01 23:50:59 +0100

    Réglage mail

--- functions/functions_forgotpw.php
+++ functions/functions_forgotpw.php
@@ -25,28 +25,30 @@
     }
 
 
-    function sendMail(){
+function sendMail(){
 
-        $token = uniqid();
-        $url = "http://wecay.alwaysdata.net/token?token=$token";
-        $sujet = "Mot de passe oublié - WeCAY";
-        $message = "Réinitialisation de votre mot de passe - WeCAY
-        Cliquez sur le lien pour réinitialiser votre mot de passe : $url
-        ---------------
-        Ceci est un mail automatique, Merci de ne pas y répondre.";
+    $token = uniqid();
+    $url = "http://wecay.alwaysdata.net/token?token=$token";
 
-        $headers = "From: noreply@wecay.com";
 
-        if(mail($_POST['mail'], $sujet, $message, $headers))
-        {
-            $db = connectDb();
-            $sql = "UPDATE utilisateur SET token = ? WHERE email = ?";
-            $stmt = $db->prepare($sql);
-            $stmt->execute([$token, $_POST['mail']]);
-            echo 'Un mail a été envoyé à votre adresse '.$_POST['mail'].'. Suivez les instructions pour modifier votre mot de passe';
-        }else
-        {
-            echo 'Une erreur est survenue...';
-        }
+    $message = "Réinitialisation de votre mot de passe - WeCAY
+    
+Cliquez sur le lien pour réinitialiser votre mot de passe : $url
+
+---------------
+Ceci est un mail automatique, Merci de ne pas y répondre.";
+
+    $headers = 'From: noreply@wecay.com';
+
+    if(mail($_POST['mail'], 'Mot de passe oublié', $message, $headers))
+    {
+        $db = connectDb();
+        $sql = "UPDATE utilisateur SET token = ? WHERE email = ?";
+        $stmt = $db->prepare($sql);
+        $stmt->execute([$token, $_POST['mail']]);
+        echo 'Un mail a été envoyé à votre adresse '.$_POST['mail'].'. Suivez les instructions pour modifier votre mot de passe';
+    }else
+    {
+        echo 'Une erreur est survenue...';
     }
-
+}
